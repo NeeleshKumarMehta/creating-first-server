5637bd03b6b4b8c652c72a7142a49f73ccd28e5d
NeeleshKumarMehta <neeleshmehta123@gmail.com>
2023-08-12 11:17:14 +0530

    installed third package party(nodemon)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8081;
 
 http.createServer((require,response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write("<h1>Hello, this is from my server</h1>");
+    response.write("<h1>Hello, this is server</h1>");
     response.end();
 })
 .listen(port, () => {
